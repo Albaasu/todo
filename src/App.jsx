81a5a775599a01b8bb3a53ce721cfc478cf6412e
ck <shibaduke4@gmail.com>
2023-03-12 22:00:07 +0900

    編集機能未実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 import React, { useState, useRef } from 'react';
 import './style.css';
-import { Button } from '@mui/material';
+import { Button, Checkbox } from '@mui/material';
 import { v4 as uuidv4 } from 'uuid';
 
 function App() {
   const [todos, setTodos] = useState([]);
-
   const todoNameRef = useRef();
 
   const handleAddTodo = (e) => {
     const name = todoNameRef.current.value;
     if (name === '') return;
+    e.preventDefault();
 
     const newTodo = {
       id: uuidv4(),
       name: name,
       completed: false,
+      status: 'incomplete',
     };
 
     setTodos((prevTodos) => {
@@ -39,15 +40,27 @@ function App() {
     setTodos(newTodos);
   };
 
+  const toggleCheck = (id) => {
+    const newList = todos.map((todo) =>
+      id === todo.id
+        ? {
+            ...todo,
+            status: todo.status === 'incomplete' ? 'completed' : 'incomplete',
+          }
+        : todo
+    );
+    setTodos(newList);
+  };
+
   return (
     <div>
-      <div className='input-area'>
+      <form className='input-area' onSubmit={handleAddTodo}>
         <p className='title'>TODOリスト</p>
         <input placeholder='TODOを入力' type='text' ref={todoNameRef} />
         <Button variant='contained' onClick={handleAddTodo}>
           追加
         </Button>
-      </div>
+      </form>
       <div className='incomplete-area'>
         <p className='title'>未完了リスト</p>
         <ul>
@@ -55,11 +68,20 @@ function App() {
             .filter((todo) => !todo.completed)
             .map((todo) => (
               <div className='list-row' key={todo.id}>
-                <li>{todo.name}</li>
+                <Checkbox
+                  style={{ marginRight: '10px' }}
+                  onClick={() => toggleCheck(todo.id)}
+                  onChange={() => {}}
+                  checked={todo.status === 'incomplete' ? false : true}
+                />
+                <li className={todo.status === 'completed' ? 'completed' : ''}>
+                  {todo.name}
+                </li>
                 <Button
                   variant='contained'
                   className='btn'
                   onClick={() => toggleTodo(todo.id)}
+                  disabled={todo.status === 'incomplete'}
                 >
                   完了
                 </Button>
@@ -77,15 +99,24 @@ function App() {
             ))}
         </ul>
       </div>
+
       <div className='complete-area'>
         <p className='title'>完了リスト</p>
         <ul>
-          <div className='list-row'>
-            <li>完了したTODO</li>
-            <Button variant='contained' className='btn'>
-              戻す
-            </Button>
-          </div>
+          {todos
+            .filter((todo) => todo.completed)
+            .map((todo) => (
+              <div className='list-row'>
+                <li>{todo.name}</li>
+                <Button
+                  variant='contained'
+                  className='btn'
+                  onClick={() => toggleTodo(todo.id)}
+                >
+                  戻す
+                </Button>
+              </div>
+            ))}
         </ul>
       </div>
     </div>
